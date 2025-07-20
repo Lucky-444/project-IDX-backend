737ec5296b63094aa6bc9379033bb7fedc153235
Lucky <sashankaswain6@gmail.com>
2025-07-20 17:29:22 +0530

    added basic setup

---
+++ backend/src/controllers/pingController.js
@@ -0,0 +1,5 @@
+export async function pingController(req, res) {
+  return res.status(200).json({
+    message: "pong", // or any other response
+  });
+}
---
+++ backend/src/index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import cors from "cors";
+import { PORT } from "./config/serverConfig.js";
+import apiRouter from './routes/index.js'
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+
+app.use('/api',apiRouter);
+
+
+
+
+app.get("/ping", (req, res) => {
+  res.json({message : "pong"});
+});
+
+
+
+app.listen(PORT, () => {
+  console.log(`server is runnig on PORT ${PORT}`);
+});
